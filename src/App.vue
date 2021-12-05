2050a6eeac46280547d2ff89b20a9b979cdafd89
Toby Scott <tscott8520@gmail.com>
2021-12-06 08:31:21 +1100

    Build basic app layout

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,45 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
-</template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+	data: function () {
+		return {
+			keycode: null
+		}
+	}
 }
 </script>
 
 <style>
 #app {
-  font-family: Montserrat;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: Montserrat;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
 }
 </style>
+
+<template>
+<div id="app" class="h-screen flex flex-col text-center">
+	<div class="bg-blue-400 text-white p-5">
+		<div class="font-bold">Article Number</div>
+		<input type="number" v-model="keycode" class="text-black rounded mt-3 px-3 py-2">
+	</div>
+
+	<div class="flex-grow max-w-screen-md mx-auto">
+
+		<div class="flex items-center bg-green-50">
+
+			<img alt="Product image" src="./assets/logo.png" class="shadow-lg rounded">
+			<div class="text-left ml-5">
+				<div class="text-2xl">Product name</div>
+				<div>{{ keycode }}</div>
+			</div>
+			
+		</div>
+
+
+	</div>
+	
+	<div class="bg-blue-400 text-white p-5">
+		<span class="font-bold">This project is not affiliated with BIG W or Woolworths Limited in any way.</span>
+	</div>
+</div>
+</template>
